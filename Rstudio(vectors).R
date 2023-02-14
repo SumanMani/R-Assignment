7a4fc2a758d6468e070756838884b85ee3d5de14
Suman Mani <109677354+SumanMani@users.noreply.github.com>
2023-02-14 11:45:24 +0530

    initial commits

---
+++ Rstudio(vectors).R
@@ -0,0 +1,24 @@
+fruits <- c("Banana",'Apple',"orange")
+fruits
+numbers <- c(1,2,3)
+numbers
+numerics <- 1:11
+numerics
+numbers1 <-1.5:6.8
+numbers1
+length(fruits)
+sort(fruits)
+sort(fruits,decreasing=TRUE)
+sort(numbers)
+length(numbers)
+fruits[1]
+fruits[c(1,3)]
+fruits[c(-1)]
+fruits[1] <- 'pear'
+fruits[2] <- 'Cherry'
+repeat_seq <- rep(c(1,2,3), times=3)
+repeat_seq
+numbers_seq <- seq(from=0, to=100, by=20)
+numbers_seq
+# Vectors
+# A vector is simply a list of items that are of the same type.
